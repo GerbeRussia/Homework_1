153753e40b4e8a529eb02516afa0df6facc22b6f
GerbeRussia <adgar830@gmail.com>
2022-12-15 20:33:25 +0700

    Задание 5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        //Циклы
+        // Задание 5
+        for (int n = 1904; n <= 2096; n = n + 4) {
+            System.out.println(n + " год является високосным");
+        }
     }
 }
