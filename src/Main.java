7a985ffb37f66def1cc44d897f879fc3720346aa
GerbeRussia <adgar830@gmail.com>
2022-12-15 22:57:27 +0700

    Задание 9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        //Циклы
+        // Задание 9
+        int savings = 29000;
+        for (int n = 1; n <= 12; n++) {
+        int savingsPerMonth = n * savings;
+        double CAPITALISM = savingsPerMonth + (savingsPerMonth * 0.12);
+            System.out.println("Месяц " + n + ", сумма накоплений равна " + CAPITALISM + " рублей");
+        }
     }
 }
