d82f2577df7ff9a4bfcd353d424e2a24c5f3ab0d
GerbeRussia <adgar830@gmail.com>
2022-12-15 23:12:24 +0700

    Задание 10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        //Циклы
+        // Задание 10
+        for( int n = 1; n <= 10; n++){
+        int operation = n * 2;
+            System.out.println("2 * " + n + " = " + operation);
+        }
     }
 }
