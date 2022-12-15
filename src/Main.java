af167053b64b065f7463590d4336bc3bcf92749d
GerbeRussia <adgar830@gmail.com>
2022-12-15 20:19:23 +0700

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        //Циклы
+        // Задание 3
+        for (int n = 2; n < 17; n = n + 2) {
+            System.out.println("Чётные числа от 0 до 17: " + n);
+        }
     }
 }
